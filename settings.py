9443e8ec7cd1a1c736f04fe87c4407771d53c1af
fahmid prachaw <fahmidprachaw@outlook.com>
2026-04-06 14:09:15 +0600

    media root updated

--- settings.py
+++ settings.py
@@ -75,7 +75,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # ─── Media Files (uploaded images) ───────────────────────────────────────────
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # ─── Default primary key ──────────────────────────────────────────────────────
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
